3e600058c9118804a1feb810842109d5fbe633dc
Leow, Max <max.leow@experian.com>
2022-04-22 19:18:40 +0800

    second draft

--- testrail_data/_category.py
+++ testrail_data/_category.py
@@ -4,7 +4,13 @@ from testrail_api._category import Results as TR_Results
 from testrail_api._category import Milestones as TR_Milestone
 from testrail_api._category import Cases as TR_Cases
 from testrail_api._category import Tests as TR_Tests
-
+from testrail_api._category import CaseFields as TR_CaseFields
+from testrail_api._category import Sections as TR_Sections
+from testrail_api._category import Template as TR_Template
+from testrail_api._category import CaseTypes as TR_CaseType
+from testrail_api._category import Priorities as TR_Priorities
+from testrail_api._category import Suites as TR_Suites
+from testrail_api._category import Statuses as TR_Statuses
 from pandas import DataFrame
 import pandas as pd
 
@@ -33,6 +39,29 @@ def auto_offset(limit_size):
     return walk
 
 
+def fill_custom_fields(project_id: int, df: DataFrame, lookup_case_field: dict):
+    def fill(x, column):
+        try:
+            lookup_custom_field = lookup_case_field[column]
+            if not x or project_id not in lookup_custom_field:
+                return ''
+            elif isinstance(x, list):
+                labels = [lookup_custom_field[project_id][i] for i in x]
+                return ','.join(labels)
+            elif isinstance(x, float) or str(x).isnumeric():
+                if str(x) == 'nan':
+                    return ''
+                x = int(x)
+                return lookup_custom_field[project_id][x]
+            return lookup_custom_field[project_id][x]
+        except KeyError:
+            print(column, x, type(x))
+            return f'UNKNOWN {x}'
+
+    for col in [c for c in df.columns if 'custom_' in c]:
+        df[col] = df[col].apply(fill, column=col)
+
+
 class Runs(TR_Runs):
 
     @auto_offset(limit)
@@ -54,18 +83,130 @@ class Plans(TR_Plans):
 
 
 class Cases(TR_Cases):
-    pass
+    def to_dataframe(self, project_id: int, suite_id: int, with_meta=False, **kwargs):
+        df = DataFrame(self.get_cases(project_id, suite_id=suite_id, **kwargs))
+        if with_meta:
+            lookup_section = Sections(self._session).get_sections_lookup(project_id, suite_id)
+            df['section_name'] = df['section_id'].apply(lambda x: lookup_section[x])
+
+            lookup_template = Template(self._session).get_template_lookup(project_id)
+            df['template_name'] = df['template_id'].apply(lambda x: lookup_template[x])
+
+            lookup_case_type = CaseTypes(self._session).get_case_types_lookup()
+            df['type_name'] = df['type_id'].apply(lambda x: lookup_case_type[x])
+
+            lookup_priority = Priorities(self._session).get_priorities_lookup()
+            df['priority_name'] = df['priority_id'].apply(lambda x: lookup_priority[x])
+
+            lookup_suite = Suites(self._session).get_suites_lookup(project_id)
+            df['suite_name'] = df['suite_id'].apply(lambda x: lookup_suite[x])
+
+            lookup_case_field = CaseFields(self._session).get_configs()
+            fill_custom_fields(project_id, df, lookup_case_field)
+
+        return df
+
+
+class Tests(TR_Tests):
+    def to_dataframe(self, run_id: int, with_meta=False, **kwargs) -> DataFrame:
+        df = DataFrame(self.get_tests(run_id, **kwargs))
+        run = Runs(self._session).get_run(run_id)
+        project_id = run['project_id']
+        if with_meta:
+            lookup_template = Template(self._session).get_template_lookup(project_id)
+            df['template_name'] = df['template_id'].apply(lambda x: lookup_template[x])
+
+            lookup_case_type = CaseTypes(self._session).get_case_types_lookup()
+            df['type_name'] = df['type_id'].apply(lambda x: lookup_case_type[x])
+
+            lookup_priority = Priorities(self._session).get_priorities_lookup()
+            df['priority_name'] = df['priority_id'].apply(lambda x: lookup_priority[x])
+
+            lookup_case_field = CaseFields(self._session).get_configs()
+            fill_custom_fields(project_id, df, lookup_case_field)
+        return df
 
 
 class Milestones(TR_Milestone):
 
-    def get_sub_milestones(self, milestone_id: int):
-        sub_mile = self.api.milestones.get_milestone(milestone_id)['milestones']
+    def get_sub_milestones(self, milestone_id: int) -> dict:
+        sub_mile = self.get_milestone(milestone_id)['milestones']
         for mile in sub_mile:
             sub_mile.extend(
                 self.get_sub_milestones(mile['id'])
             )
         return sub_mile
+
+    def sub_milestones_to_dataframe(self, milestone_id: int):
+        return DataFrame(self.get_sub_milestones(milestone_id))
+
+
+class Sections(TR_Sections):
+    def to_dataframe(self, project_id: int, suite_id: int, **kwargs) -> DataFrame:
+        return DataFrame(self.get_sections(project_id=project_id, suite_id=suite_id, **kwargs))
+
+    def get_sections_lookup(self, project_id: int, suite_id: int) -> dict:
+        df = self.to_dataframe(project_id, suite_id)
+        return dict(zip(df['id'], df['name']))
+
+
+class Template(TR_Template):
+    def to_dataframe(self, project_id: int) -> DataFrame:
+        return pd.DataFrame(self.get_templates(project_id))
+
+    def get_template_lookup(self, project_id: int) -> dict:
+        df = self.to_dataframe(project_id)
+        return dict(zip(df['id'], df['name']))
+
+
+class CaseFields(TR_CaseFields):
+    def get_configs(self) -> dict:
+        df = DataFrame(self.get_case_fields()).filter(['system_name', 'configs'])
+
+        def get_df_by_system_name(name: str):
+            df_config = pd.DataFrame(df.query(f'system_name == "{name}"')['configs'].to_list()[0])
+            df_context = pd.DataFrame(df_config['context'].to_list())
+            df_option = pd.DataFrame(df_config['options'].to_list())
+            df_merged = df_context.join(df_option)
+            return df_merged
+
+        def split_by_comma(i: str, index):
+            if i:
+                items = i.split(',')
+                return int(items[index]) if items[index].isnumeric() else items[index]
+            return 0
+
+        lookup = {}
+        for _, row_i in df.iterrows():
+            system_name = row_i['system_name']
+            options = {}
+            lookup[system_name] = options
+            for _, row_j in get_df_by_system_name(system_name).iterrows():
+                if 'items' in row_j:
+                    for pid in row_j['project_ids']:
+                        options[pid] = {split_by_comma(r, 0): split_by_comma(r, 1)
+                                        for r in row_j['items'].split('\n')}
+                options.update({None: '', '': ''})
+
+        return lookup
+
+
+class CaseTypes(TR_CaseType):
+    def to_dataframe(self) -> DataFrame:
+        return DataFrame(self.get_case_types())
+
+    def get_case_types_lookup(self) -> dict:
+        df = self.to_dataframe()
+        return dict(zip(df['id'], df['name']))
+
+
+class Priorities(TR_Priorities):
+    def to_dataframe(self) -> DataFrame:
+        return DataFrame(self.get_priorities())
+
+    def get_priorities_lookup(self) -> dict:
+        df = self.to_dataframe()
+        return dict(zip(df['id'], df['name']))
 
 
 class Results(TR_Results):
@@ -87,5 +228,23 @@ class Results(TR_Results):
         df_runs = Runs(self._session).to_dataframe(
             project_id=project_id, milestone_id=milestone_id)
         print(df_runs)
-        results = [self.dataframe_from_run(id, **kwargs) for id in df_runs['id'].to_list()]
+        results = [self.dataframe_from_run(run_id, **kwargs) for run_id in df_runs['id'].to_list()]
         return pd.concat(results, sort=False)
+
+
+class Suites(TR_Suites):
+    def to_dataframe(self, project_id: int):
+        return DataFrame(self.get_suites(project_id))
+
+    def get_suites_lookup(self, project_id: int) -> dict:
+        df = self.to_dataframe(project_id)
+        return dict(zip(df['id'], df['name']))
+
+
+class Statuses(TR_Statuses):
+    def to_dataframe(self) -> DataFrame:
+        return DataFrame(self.get_statuses())
+
+    def get_statuses_lookup(self, column='name') -> dict:
+        df = self.to_dataframe()
+        return dict(zip(df['id'], df[column]))
--- testrail_data/_testrail_api.py
+++ testrail_data/_testrail_api.py
@@ -1,5 +1,19 @@
 from testrail_api import TestRailAPI as TRApi
-from testrail_data._category import Runs, Plans, Results, Milestones
+from testrail_data._category import (
+    Runs,
+    Plans,
+    Results,
+    Milestones,
+    Cases,
+    CaseFields,
+    Sections,
+    Template,
+    CaseTypes,
+    Priorities,
+    Suites,
+    Statuses,
+    Tests,
+)
 
 
 class TestRailAPI(TRApi):
@@ -20,5 +34,41 @@ class TestRailAPI(TRApi):
         return Results(self)
 
     @property
+    def cases(self) -> Cases:
+        return Cases(self)
+
+    @property
     def milestones(self) -> Milestones:
         return Milestones(self)
+
+    @property
+    def case_fields(self) -> CaseFields:
+        return CaseFields(self)
+
+    @property
+    def sections(self) -> Sections:
+        return Sections(self)
+
+    @property
+    def templates(self) -> Template:
+        return Template(self)
+
+    @property
+    def case_types(self) -> CaseTypes:
+        return CaseTypes(self)
+
+    @property
+    def priorities(self) -> Priorities:
+        return Priorities(self)
+
+    @property
+    def suites(self) -> Suites:
+        return Suites(self)
+
+    @property
+    def statuses(self) -> Statuses:
+        return Statuses(self)
+
+    @property
+    def tests(self) -> Tests:
+        return Tests(self)
