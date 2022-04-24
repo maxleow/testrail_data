8b03a485f119e6b75fd00ecbfe48658d63c789db
Leow, Max <max.leow@experian.com>
2022-04-24 17:53:48 +0800

    change to dynamic setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testrail-data",
+    version="0.0.4",
+    install_requires=[
+        "pandas",
+        "testrail-api>=1.10",
+    ],
+    author="Max Leow",
+    author_email="maxengiu@outlook.com",
+    description="Pandas DataFrame integrated API wrapper for Testrail",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maxleow/testrail_data",
+    project_urls={
+        "Bug Tracker": "https://github.com/maxleow/testrail_data/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    python_requires=">=3.7",
+)
